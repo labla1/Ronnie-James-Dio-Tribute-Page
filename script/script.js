e290500e42b3658efca479ef34679191abeb33ca
Piotr Grzegorzynski <pgrzegorzynski@gmail.com>
2020-06-27 19:27:27 +0200

    js refactored code (functions)

--- script/script.js
+++ script/script.js
@@ -1,85 +1,91 @@
-const welcome = () => {
-  console.log("Hello!");
-}
-welcome  ();
+{
+  const welcome = () => {
+    console.log("Hello!");
+  }
+  welcome();
 
-let changeColor = document.querySelector(".changeColor");
-let container = document.querySelector(".js-container");
+  // Change Background Theme on Button Click Function
+  const changeColor = document.querySelector(".changeColor");
+  const container = document.querySelector(".container");
 
-container.classList.remove("js-container");
-
-changeColor.addEventListener("click", () => {
+  const onChangeBackgroundClick = () => {
     if (changeColor.innerText === "Switch to Grey Theme") {
-        changeColor.innerText = "Switch to Crimson Theme";
+      changeColor.innerText = "Switch to Crimson Theme";
     } else {
-        changeColor.innerText = "Switch to Grey Theme";
-    } 
-    container.classList.toggle("container")
-});
+      changeColor.innerText = "Switch to Grey Theme";
+    }
+    container.classList.toggle("container");
+  }
+  changeColor.addEventListener("click", onChangeBackgroundClick);
 
-let changePicture = document.querySelector(".changePicture");
-let firstPicture = document.querySelector(".image1");
-let secondPicture = document.querySelector(".newClass");
-let img1 = "https://goingapp.pl/storage/app/uploads/public/5a6/f6c/5b7/thumb_173841_600x350_0_0_crop.jpg";
-let img2 = "https://digitalage.com.tr/wp-content/uploads/2017/07/Ronnie-James-Dio-hologramla-sahnelere-geri-donuyor.jpg"
+  //Change Image on Button Click Function
+  const changePicture = document.querySelector(".changePicture");
 
-changePicture.addEventListener("click", () => {
+  const onChangeImageClick = () => {
+    const firstPicture = document.querySelector(".image1");
+    const img1 = "https://goingapp.pl/storage/app/uploads/public/5a6/f6c/5b7/thumb_173841_600x350_0_0_crop.jpg";
+    const img2 = "https://digitalage.com.tr/wp-content/uploads/2017/07/Ronnie-James-Dio-hologramla-sahnelere-geri-donuyor.jpg";
+
     if (firstPicture.src === img1) {
-        firstPicture.src = img2 ;
-        changePicture.innerText = "Load Previous Picture";
+      firstPicture.src = img2;
+      changePicture.innerText = "Load Previous Picture";
     } else {
-        firstPicture.src = img1;
-        changePicture.innerText = "Load Another Picture";
+      firstPicture.src = img1;
+      changePicture.innerText = "Load Another Picture";
     }
     firstPicture.classList.toggle("newClass");
-})
+  }
+  changePicture.addEventListener("click", onChangeImageClick);
 
-
-console.log(changePicture);
-
-let button = document.querySelector(".button2");
-let random = document.querySelector(".generatedTitle");
-let span = document.querySelector(".spanDisplay");
-
-button.addEventListener("click", () => {
+  // Generate Random Song on Click Function
   
-  let input = prompt("Enter the number between 1 and 10");
-  span.classList.remove("spanDisplay");
-  
-  switch (input) {
-    default:
-      prompt("Enter valid number '(1-10)'");
-      break;
-    case "1":
-      random.innerText = "♪ Don't Talk to Strangers ♪";
-      break;
-    case "2":
-      random.innerText = "♪ Straight Through the Heart ♪";
-      break;
-    case "3":
-      random.innerText = "♪ Dream On ♪";
-      break;
-    case "4":
-      random.innerText = "♪ Rock 'n' Roll Children ♪";
-      break;
-    case "5":
-      random.innerText = "♪ The Last in Line ♪";
-      break;
-    case "6":
-      random.innerText = "♪ We Rock ♪";
-      break;
-    case "7":
-      random.innerText = "♪ All the Fools Sailed Away ♪";
-      break;
-    case "8":
-      random.innerText = "♪ Hungry for Heaven ♪";
-      break;
-    case "9":
-      random.innerText = "♪ Stand Up and Shout ♪";
-      break;
-    case "10":
-      random.innerText = "♪ Sacred Heart ♪";
-      break;
-  } 
-});
+  const button = document.querySelector(".button2");
 
+  const generateRandomSongOnClick = () => {
+    const span = document.querySelector(".spanDisplay");
+    const random = document.querySelector(".generatedTitle");
+    
+    const input = prompt("Enter the number between 1 and 10");
+    span.classList.remove("spanDisplay");
+
+    switch (input) {
+      default:
+        prompt("Enter valid number '(1-10)'");
+        break;
+      case "1":
+        random.innerText = "♪ Don't Talk to Strangers ♪";
+        break;
+      case "2":
+        random.innerText = "♪ Straight Through the Heart ♪";
+        break;
+      case "3":
+        random.innerText = "♪ Dream On ♪";
+        break;
+      case "4":
+        random.innerText = "♪ Rock 'n' Roll Children ♪";
+        break;
+      case "5":
+        random.innerText = "♪ The Last in Line ♪";
+        break;
+      case "6":
+        random.innerText = "♪ We Rock ♪";
+        break;
+      case "7":
+        random.innerText = "♪ All the Fools Sailed Away ♪";
+        break;
+      case "8":
+        random.innerText = "♪ Hungry for Heaven ♪";
+        break;
+      case "9":
+        random.innerText = "♪ Stand Up and Shout ♪";
+        break;
+      case "10":
+        random.innerText = "♪ Sacred Heart ♪";
+        break;
+    }
+  }
+
+  button.addEventListener("click", generateRandomSongOnClick);
+
+
+}
